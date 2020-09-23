ebc19610f8ef6223a3e88b702a7d7634d15a9613
Kim G <Kim_greenbush@icloud.com>
2020-09-23 11:39:28 -0700

    changed quotes on url

--- WttArcade/WttArcade/urls.py
+++ WttArcade/WttArcade/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('Arcade.urls')),
-    path('snake/', include("Snake.urls"))
+    path('snake/', include('Snake.urls'))
 ]
