ff2d45e371eb634f26ab4dbdb8105636fb49befa
Kim G <Kim_greenbush@icloud.com>
2020-09-23 18:03:13 -0700

    index styled and routed

--- WttArcade/apps/Arcade/urls.py
+++ WttArcade/apps/Arcade/urls.py
@@ -5,5 +5,8 @@ app_name='Arcade'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
     path('dashboard/<int:player_id>/', views.dashboard, name='dashboard'),
 ]
