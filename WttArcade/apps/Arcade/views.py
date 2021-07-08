fe5c3978c35c8ff2116f5f5ac9d0038f17b1a5fb
Kim Greenbush <kim_greenbush@icloud.com>
2021-07-08 16:13:10 -0700

    add permanent redirects for status code 301

--- WttArcade/apps/Arcade/views.py
+++ WttArcade/apps/Arcade/views.py
@@ -14,7 +14,7 @@ def index(request):
 
 def dashboard(request, player_id):
     if 'uuid' not in request.session:
-        return redirect('/')
+        return redirect('/', permanent=True)
     context = {
         "logged_player": Player.objects.get(id=request.session['uuid']),
         "player": Player.objects.get(id=player_id),
@@ -24,18 +24,18 @@ def dashboard(request, player_id):
         "pacman": Game.objects.filter(player =  Player.objects.get(id=player_id), title="pacman").order_by("-score"),
         "tetris": Game.objects.filter(player =  Player.objects.get(id=player_id), title="tetris").order_by("-score")
     }
-    return render(request, 'dashboard.html', context)
+    return render(request, 'dashboard.html', context, permanent=True)
 
 def arcade(request):
     if 'uuid' not in request.session:
-        return redirect('/')
+        return redirect('/', permanent=True)
     context = {
         "player": Player.objects.get(id=request.session['uuid']),
         "snake_scores": Game.objects.filter(title="snake").order_by("-score"),
         "pacman_scores": Game.objects.filter(title="pacman").order_by("-score"),
         "tetris_scores": Game.objects.filter(title="tetris").order_by("-score")
     }
-    return render(request, 'arcade.html', context)
+    return render(request, 'arcade.html', context, permanent=True)
 
 # Register/Login/Logout
 def register(request):
@@ -43,7 +43,7 @@ def register(request):
     if len(errors) > 0:
         for key, value in errors.items():
             messages.warning(request, value)
-        return redirect('/')
+        return redirect('/', permanent=True)
     else:
         password = request.POST['password']
         pw_hash = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
@@ -52,37 +52,37 @@ def register(request):
         player.save()
         logged_player = player.id
         request.session['uuid'] = logged_player
-        return redirect(f'/arcade/dashboard/{logged_player}/')
+        return redirect(f'/arcade/dashboard/{logged_player}/', permanent=True)
 
 def login(request):
     errors = Player.objects.login_validator(request.POST)
     if len(errors) > 0:
         for key, value in errors.items():
             messages.error(request, value)
-        return redirect('/')
+        return redirect('/', permanent=True)
     else:
         player = Player.objects.filter(email=request.POST['email'])
         logged_player = player[0].id
         request.session['uuid'] = logged_player
-        return redirect(f'/arcade/dashboard/{logged_player}/')
+        return redirect(f'/arcade/dashboard/{logged_player}/', permanent=True)
 
 def logout(request):
     request.session.flush()
-    return redirect('/')
+    return redirect('/', permanent=True)
 
 # Redirect/Process
 def dashboard_redirect(request):
     if 'uuid' not in request.session:
-        return redirect('/')
+        return redirect('/', permanent=True)
     logged_player = request.session['uuid']
-    return redirect(f'/arcade/dashboard/{logged_player}/')
+    return redirect(f'/arcade/dashboard/{logged_player}/', permanent=True)
 
 def add_friend(request, player_id):
     Player.objects.get(id=request.session['uuid']).friendships.add(Player.objects.get(id=player_id))
     Player.objects.get(id=request.session['uuid']).friends.add(Player.objects.get(id=player_id))
-    return redirect(f'/arcade/dashboard/{player_id}/')
+    return redirect(f'/arcade/dashboard/{player_id}/', permanent=True)
 
 def remove_friend(request, player_id):
     Player.objects.get(id=request.session['uuid']).friendships.remove(Player.objects.get(id=player_id))
     Player.objects.get(id=request.session['uuid']).friends.remove(Player.objects.get(id=player_id))
-    return redirect(f'/arcade/dashboard/{player_id}/')
+    return redirect(f'/arcade/dashboard/{player_id}/', permanent=True)
