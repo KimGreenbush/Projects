d141283be86e6aa531e1e0a77ce850ac088a2508
Kim Greenbush <kim_greenbush@icloud.com>
2021-07-08 22:29:33 -0700

    did some refactoring

--- WttArcade/apps/Arcade/tests/test_loginReg_models.py
+++ WttArcade/apps/Arcade/tests/test_loginReg_models.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 import bcrypt
 from apps.Arcade.models import Player
 
-def create_test_player(username, email, password, pass_confirm):
+def create_test_player(username="Kim", email="test@test.com", password="Secure123", pass_confirm="Secure123"):
     player = {
         "username" : username,
         "email" : email,
@@ -11,14 +11,14 @@ def create_test_player(username, email, password, pass_confirm):
     }
     return Player.objects.register_validator(player)
 
-def validate_test_player(email, password):
+def validate_test_player(email="test@test.com", password="Secure123"):
     user = {
         "email": email,
         "password": password
     }
     return Player.objects.login_validator(user)
 
-class RegistrationValidationTestCase(TestCase):
+class RegistrationValidationTest(TestCase):
     def setUp(self):
         self.player = Player.objects.create(username="Kim", email="test1@test.com", password="Secure123")
 
@@ -26,48 +26,48 @@ class RegistrationValidationTestCase(TestCase):
         self.assertIsNotNone(self.player)
 
     def test_validate_user(self):
-        a_user = create_test_player("Kim", "test@test.com", "Secure123", "Secure123")
+        a_user = create_test_player()
         self.assertEquals(len(a_user), 0)
 
     def test_username_less_than_three(self):
-        short_username = create_test_player("Ki", "test@test.com", "Secure123", "Secure123")
+        short_username = create_test_player(username="Ki")
         self.assertGreater(len(short_username), 0)
 
     def test_username_greater_than_nine(self):
-        long_username = create_test_player("Kimberleyy", "test@test.com", "Secure123", "Secure123")
+        long_username = create_test_player(username="Kimberleyy")
         self.assertGreater(len(long_username), 0)
 
     def test_email_isUnavailable(self):
         Player.objects.create(username="Kim", email="test@test.com", password="Secure123")
-        unavailable_email = create_test_player("Kimberley", "test@test.com", "Secure123", "Secure123")
+        unavailable_email = create_test_player()
         self.assertGreater(len(unavailable_email), 0)
 
     def test_email_regex(self):
-        bad_email = create_test_player("Kimberley", "test@test", "Secure123", "Secure123")
+        bad_email = create_test_player(email="test@test")
         self.assertGreater(len(bad_email), 0)
 
     def test_password_less_than_eight(self):
-        bad_pass = create_test_player("Kimberley", "test@test.com", "NotSafe", "Secure123")
+        bad_pass = create_test_player(password="NotSafe")
         self.assertGreater(len(bad_pass), 0)
 
     def test_confirm_password_match(self):
-        pass_not_matching = create_test_player("Kimberley", "test@test.com", "NotSafe", "Secure123")
+        pass_not_matching = create_test_player(password="NotSafe", pass_confirm="Secure123")
         self.assertGreater(len(pass_not_matching), 0)
 
 
-class LoginValidationTestCase(TestCase):
+class LoginValidationTest(TestCase):
     def test_no_player(self):
-        not_a_registered_email = validate_test_player("notanemail@notadomain.com", "Secure123")
+        not_a_registered_email = validate_test_player(email="notanemail@notadomain.com")
         self.assertGreater(len(not_a_registered_email), 0)
 
     def test_player_password_does_not_match_input(self):
         pw_hash = bcrypt.hashpw("Secure123".encode(), bcrypt.gensalt()).decode()
         Player.objects.create(username="Kim", email="test@test.com", password=pw_hash)
-        invalid_pass = validate_test_player("test@test.com", "Hackers1!")
+        invalid_pass = validate_test_player(password="Hackers1!")
         self.assertGreater(len(invalid_pass), 0)
 
     def test_player_password_matches_input(self):
         pw_hash = bcrypt.hashpw("Secure123".encode(), bcrypt.gensalt()).decode()
         Player.objects.create(username="Kim", email="test@test.com", password=pw_hash)
-        valid_pass = validate_test_player("test@test.com", "Secure123")
+        valid_pass = validate_test_player()
         self.assertEquals(len(valid_pass), 0)
--- WttArcade/apps/Arcade/tests/test_views.py
+++ WttArcade/apps/Arcade/tests/test_views.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
-from apps.Arcade.models import Player
+from ..models import Player
 import bcrypt
 
-def create_test_player(username, email, password, pass_confirm):
+def create_test_player(username="Kimberley", email="test@test.com", password="Secure123", pass_confirm="Secure123"):
     return {
         "username": username,
         "email": email,
@@ -10,35 +10,28 @@ def create_test_player(username, email, password, pass_confirm):
         "pass_confirm": pass_confirm
     }
 
-class RegistrationViewTestCase(TestCase):
-    # def test_successful_registration_redirect(self):
-    #     new_player = create_test_player(
-    #         "Kimberley",
-    #         "test1@test1.com",
-    #         "Secure123",
-    #         "Secure123")
-    #     response = self.client.post("/register/", new_player)
-    #     self.assertRedirects(response, response.url, status_code=301, target_status_code=200)
+class RegistrationViewTest(TestCase):
+    def test_successful_registration_redirect(self):
+        new_player = create_test_player()
+        response = self.client.post("/register/", new_player)
+        self.assertRedirects(response, response.url, status_code=301, target_status_code=200)
 
-    # def test_unsuccessful_registration_redirect(self):
-    #     new_player = create_test_player(
-    #         "Kimberley",
-    #         "test1@test1.com",
-    #         "Secure123",
-    #         "Secure123")
-    #     response = self.client.post("/register/", new_player)
-    #     self.assertRedirects(response, response.url, status_code=301, target_status_code=200)
+    def test_unsuccessful_registration_redirect(self):
+        new_player = create_test_player(pass_confirm="Secure456")
+        response = self.client.post("/register/", new_player)
+        self.assertRedirects(response, "/", status_code=301, target_status_code=200)
 
-    # def test_successful_login_redirect(self):
-        # pw_hash = bcrypt.hashpw("Secure123".encode(), bcrypt.gensalt()).decode()
-        # Player.objects.create(username="Kim", email="test@test.com", password=pw_hash)
-        # existing_player = create_test_player("Kim", "test@test.com", "Secure123", "Secure123")
-        # response = self.client.post("/login/", existing_player)
-    #     self.assertRedirects(response, response.url, status_code=301, target_status_code=200)
-
-    def test_unsuccessful_login_redirect(self):
+class LoginViewTest(TestCase):
+    def setUp(self):
         pw_hash = bcrypt.hashpw("Secure123".encode(), bcrypt.gensalt()).decode()
-        Player.objects.create(username="Kim", email="test@test.com", password=pw_hash)
-        existing_player = create_test_player("Kim", "test@test.com", "Secure456", "Secure456")
+        self.player1 =  Player.objects.create(username="Kim", email="test@test.com", password=pw_hash)
+
+    def test_successful_login_redirect(self):
+        existing_player = create_test_player()
         response = self.client.post("/login/", existing_player)
         self.assertRedirects(response, response.url, status_code=301, target_status_code=200)
+
+    def test_unsuccessful_login_redirect(self):
+        existing_player = create_test_player(password="Secure456")
+        response = self.client.post("/login/", existing_player)
+        self.assertRedirects(response, "/", status_code=301, target_status_code=200)
